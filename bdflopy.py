c381df57fb28a7ae47367df31a3a52055f44438e
KONRADSPC\khafe <khafen74@gmail.com>
2018-01-02 11:42:01 -0800

    setting up file paths for use in flopy

--- bdflopy.py
+++ bdflopy.py
@@ -7,34 +7,54 @@ import csv
 from scipy import ndimage
 
 class BDflopy:
-    def __init__(self, modflowexe, outdir, maindir, outcsv = None, batch = False):
+    def __init__(self, modflowexe, indir, modeldir, outdir, demfilename):
         self.modflowexe = modflowexe
+        self.indir = indir
+        self.modeldir = modeldir
         self.outdir = outdir
-        self.maindir = maindir
-        self.batch = batch
-        self.setVariables(outcsv)
+        self.setVariables()
+        self.setPaths()
 
-    def setVariables(self, outcsv):
-        self.setCSVpath(outcsv)
+    def setPaths(self):
+        self.setWSEPaths()
+        self.setPondDepthPaths()
+
+    def setVariables(self, demfilename):
+        self.driver = gdal.GetDriverByName('GTiff')
+        self.dempath = self.indir + "/" + demfilename
+        demds = gdal.Open(self.dempath)
+        self.geot = demds.GetGeoTransform()
+        self.prj = demds.GetProjection()
+        self.xsize = demds.RasterXSize
+        self.ysize = demds.RasterYSize
+        demds = None
         self.mf = []
         self.mfnames = ["start", "lo", "mid", "hi"]
         for mfname in self.mfnames:
             self.mf.append(flopy.modflow.Modflow(mfname, exe_name = self.modflowexe))
 
-    def setCSVpath(self, outcsv):
-        if outcsv == None:
-            self.outcsv = self.outdir + "/summary.csv"
-        else:
-            self.outcsv = outcsv
+    def setHeadPaths(self):
+        self.headPaths = []
+        self.headPaths.append(self.modeldir + "/head_start.tif")
+        self.headPaths.append(self.modeldir + "/head_lo.tif")
+        self.headPaths.append(self.modeldir + "/head_mid.tif")
+        self.headPaths.append(self.modeldir + "/head_hi.tif")
 
-    def setPaths(self, startWSE):
-        self.indir = self.maindir + "/02_rasIn"
-        self.modeldir = self.maindir + "03_out"
-        demPaths = []
+    def setPondDepthPaths(self):
+        self.pondPaths = []
+        self.pondPaths.append(self.modeldir + "/depLo.tif")
+        self.pondPaths.append(self.modeldir + "/depMid.tif")
+        self.pondPaths.append(self.modeldir + "/depHi.tif")
 
-    def setPathsBatch(self):
+    def setWSEPaths(self):
+        self.wsePaths = []
+        self.wsePaths.append(self.dempath)
+        self.wsePaths.append(self.modeldir + "/WSESurf_lo.tif")
+        self.wsePaths.append(self.modeldir + "/WSESurf_mid.tif")
+        self.wsePaths.append(self.modeldir + "/WSESurf_hi.tif")
 
     def run(self):
 
 
-    def runBatch(self):
+
+
